ef31b23a4d767c9aa782d7ef39468109ef587fb7
George Sideris <siderisg4@hotmail.gr>
2024-02-18 00:06:45 +0200

    create homepage

---
+++ resources/views/includes/scripts.blade.php
@@ -0,0 +1,16 @@
+<script src="{{ asset('assets/js/plugins/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/jqueryui.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/counter-up.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/swiper.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/metismenu.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/waypoint.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/waw.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/gsap.min.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/scrolltigger.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/split-text.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/contact.form.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/split-type.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/jquery-timepicker.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/bootstrap.min.js') }}"></script>
+
+<script src="{{ asset('assets/js/main.js') }}"></script>
---
+++ resources/views/layouts/apps.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    @include('includes.head')
+    <body class="index-two">
+        @yield('content')
+        <!-- jquery js -->
+        @include('includes.scripts')
+        <!-- header style two End -->
+    </body>
+</html>
+
